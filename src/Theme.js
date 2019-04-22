8b71463681ce797fddecd5e442c58619251c3cff
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-22 17:15:26 +0200

    Add Theme.js file + import in App.js

---
+++ src/Theme.js
@@ -0,0 +1,26 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import orange from "@material-ui/core/colors/orange";
+import brown from "@material-ui/core/colors/brown";
+
+const light = 50;
+const main = 500;
+const dark = 800;
+
+const MyTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: orange[light],
+      main: orange[main],
+      dark: orange[dark],
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: brown[light],
+      main: brown[main],
+      dark: brown[dark],
+      contrastText: "#000"
+    }
+  }
+});
+
+export default MyTheme;
