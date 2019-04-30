ea2e5c8da14c02ee01331ddeed47c01019f7cddb
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-04-30 19:24:39 +0200

    Footer update + app css update for the footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,48 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
-  root: {
-    flexGrow: 1
-  },
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
+	root: {
+		flexGrow: 1,
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
 };
 
 function Navbar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <img
-            className="icon"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Creative-Tail-Animal-fox.svg/128px-Creative-Tail-Animal-fox.svg.png"
-            alt="fox"
-          />
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            QuizFox
-          </Typography>
-          <Button color="inherit">About us</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+	const { classes } = props;
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<img
+						className="icon"
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Creative-Tail-Animal-fox.svg/128px-Creative-Tail-Animal-fox.svg.png"
+						alt="fox"
+					/>
+					<Typography variant="h6" color="inherit" className={classes.grow}>
+						<NavLink activeClassName="active" exact to="/">
+							Home
+						</NavLink>
+					</Typography>
+					<Typography variant="h6" color="inherit" className={classes.grow}>
+						<NavLink activeClassName="active" to="/about-us">
+							About Us
+						</NavLink>
+					</Typography>
+					{/* <Button color="inherit">About us</Button> */}
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
-
-Navbar.propTypes = {
-  classes: PropTypes.object.isRequired
-};
 
 export default withStyles(styles)(Navbar);
