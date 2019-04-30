409bc2f507d5505f8c83d5823f701bc014b154c4
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-30 10:59:34 +0200

    udapte AboutUS page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,1 +1,23 @@
 import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <section>
+        <div>
+          <img />
+          <p>Raphael</p>
+        </div>
+        <div>
+          <img />
+          <p>Laura</p>
+        </div>
+        <div>
+          <img />
+          <p>Camille</p>
+        </div>
+      </section>
+    </div>
+  );
+};
