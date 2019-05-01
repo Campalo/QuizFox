265286500be1ff9285db5dfe5f6b2f7d7ae05368
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-01 12:17:44 +0200

    add some designs to Home(landingpage) and Category pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,6 @@ function Header(props) {
       {/* background image in css file */}
       <h1>Welcome to QuizFox!</h1>
       <h3>A Trivial Pursuit-like game created during a React bootcamp</h3>
-      <p>Eager to play? Choose one category and its level of difficulty to access the questions</p>
       <MyButton>Play now</MyButton>
     </header>
   );
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -8,6 +8,8 @@ class Home extends Component {
     return (
       <div>
         <Navbar2 />
+        <h1>Eager to play?</h1>
+        <h3>Choose one category and its level of difficulty to access the questions</h3>
         <section id="categoryCards">
           <CategoryCard />
           <CategoryCard />
