6f95f19f0495ea3ebaf81fe103c05db4b4a7b6a3
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-05-08 18:43:37 +0200

    added function to bring all answers in  array + function to shuffle answers and wrap them into our MyButton

--- src/components/DisplayQuestion.js
+++ src/components/DisplayQuestion.js
@@ -1,38 +1,36 @@
-import React from "react";
+import React from 'react';
+import MyButton from './Button';
 
 const DisplayQuestion = ({ showQuestion, currentQuestion, amount }) => {
-  return (
-    <div>
-      <h5>{showQuestion.question}</h5>
-      <p>{showQuestion.correct_answer}</p>
-      <p>{showQuestion.incorrect_answers}</p>
-      <p>
-        You are on Question {currentQuestion} on {amount}
-      </p>
-    </div>
-  );
+	let finalList = [];
+
+	const answersList = [].concat(showQuestion.correct_answer).concat(showQuestion.incorrect_answers);
+	console.log('our array:', answersList);
+
+	for (let i = 0; i < answersList.length; i++) {
+		finalList.push(<MyButton>{answersList[i]}</MyButton>);
+		console.log(finalList);
+	}
+
+	function shuffle(finalList) {
+		for (let i = finalList.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[finalList[i], finalList[j]] = [finalList[j], finalList[i]];
+		}
+		return finalList;
+	}
+
+	return (
+		<div>
+			<h5>{showQuestion.question}</h5>
+			<p>{showQuestion.correct_answer}</p>
+			{/* <p>{showQuestion.incorrect_answers}</p> */}
+			<p>
+				You are on Question {currentQuestion} on {amount}
+			</p>
+			<p>{shuffle(finalList)}</p>
+		</div>
+	);
 };
 
 export default DisplayQuestion;
-
-// TO DO :
-
-// const Answers = ({ correct_answer, incorrect_answers }) => {
-//   let list = [];
-
-//   const answerList = [].concat(correct_answer).concat(incorrect_answers);
-//   console.log("our array:", answerList);
-//   for (let i = 0; i <= answerList.length; i++) {
-//     list.push(answerList[i]);
-//   }
-
-//   // shuffle() => {}
-
-//   return (
-//     <div>
-//       {" "}
-//       Array : {list}
-//       {/* {shuffle(list)} */}
-//     </div>
-//   );
-// };
