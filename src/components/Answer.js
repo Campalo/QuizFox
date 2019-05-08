96b2786085ef7451a4669f3d2c80e4ad30ff6bd4
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-08 15:38:52 +0200

    delete those files to refactor

--- src/components/Answer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Answer = () => {
-  return <h1>hi</h1>;
-};
-
-export default Answer;
--- src/components/Question.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Answer = () => {
-  return <h1>hi question</h1>;
-};
-
-export default Answer;
