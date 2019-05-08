5156da72e6fce573372cda16dab8134d60ec1f4f
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-05-08 16:38:39 +0200

    deleted minor typos and updated the DisplayResult component + Quiz component

--- src/components/DisplayResult.js
+++ src/components/DisplayResult.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const displayResult = (correctAnswer, amount) => {
-	let congrates = '';
-	if (this.state.correctAnswer <= 5) {
-		return (congrats = 'Almost there, try again to improve your Skill!💡');
-	} else if (this.state.correctAnswer <= 8) {
-		return (congrats = 'Nice try, almost perfect!🤓');
-	} else if (this.state.correctAnswer <= 10) {
-		return (congrats = 'Amazing, smart Fox!🦊');
+const DisplayResult = showQuestion => {
+	let congrats = '';
+	if (showQuestion.correctAnswer <= 5) {
+		congrats = 'Almost there, try again to improve your Skill!💡';
+	} else if (showQuestion.correctAnswer <= 8) {
+		congrats = 'Nice try, almost perfect!🤓';
+	} else if (showQuestion.correctAnswer <= 10) {
+		congrats = 'Amazing, smart Fox!🦊';
 	} else {
-		return (congrats = 'Somehting went wrong little Hacker 😉');
+		congrats = 'Something went wrong little Hacker 😉';
 	}
+	return <div>{congrats}</div>;
 };
 
-return <div>{congrats}</div>;
-
-export default displayResult;
+export default DisplayResult;
