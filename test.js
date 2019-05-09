4aceea6d633964b0d84fea020de1351cbef69a43
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-05-09 17:19:44 +0200

    third test

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ alert('help me iam confused');
 
 const myFunction = () => {
   if (true) {
-    return 'Good!';
+    return 'Good + but also not!';
+  }
+  let arrList = [];
+  for (let i = 0; i < 0; i++) {
+    arrList.push[i];
   }
 };
