3cd68c6a58479a4f655101084c71ff6e454d1ed3
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-13 12:03:52 +0200

    reduce size of MyButton to fit in CategoryCards

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
-import "../App.css";
-import MyButton from "./Button";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import '../App.css';
+import MyButton from './Button';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
