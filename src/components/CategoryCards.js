4a1a0b8ab342e113652f0fe1eab36d73dea45444
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-13 12:05:33 +0200

    add states and props to update Cat No, img, name

--- src/components/CategoryCards.js
+++ src/components/CategoryCards.js
@@ -1,47 +1,45 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import "../App.css";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import '../App.css';
+import { NavLink } from 'react-router-dom';
+import MyButton from './Button';
 
-function CategoryCard() {
+function CategoryCard({ category, name, photo }) {
   return (
     <Card id="responsiveCards">
       <CardActionArea>
-        <CardMedia id="mediaCards" image="https://i.imgur.com/1MLvmOW.jpg" title="Little fox" />
+        <CardMedia id="mediaCards" image={photo} title={name} />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2" color="secondary">
-            First Category
+          <Typography gutterBottom variant="h5" component="h2" color="primary">
+            {name}
           </Typography>
-          <Typography component="p">
-            Do you like XXX then this First Category is for you, give it a try!
-          </Typography>
-          <Typography component="p" color="secondary">
-            <b>Chose your difficulty:</b>
+          <Typography component="h2" color="secondary">
+            Chose your difficulty:
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
         {/* Use the Url Parameters to set the category and difficulty in the url
-        and make those parameters available for the Quiz component.
-        TO DO : make the category parameter dynamique */}
-        <Button size="large" color="primary">
-          <NavLink to="/quiz/18/easy">Easy</NavLink>
-        </Button>
-        <Button size="large" color="primary">
-          <NavLink to="/quiz/18/medium">Medium</NavLink>
-        </Button>
-        <Button size="large" color="primary">
-          <NavLink to="/quiz/18/hard">Hard</NavLink>
-        </Button>
+        and make those parameters available for the Quiz component.*/}
+        <MyButton>
+          <NavLink to={'/quiz/' + category + '/easy'}>Easy</NavLink>
+        </MyButton>
+        <MyButton>
+          <NavLink to={'/quiz/' + category + '/medium'}>Medium</NavLink>
+        </MyButton>
+        <MyButton>
+          <NavLink to={'/quiz/' + category + '/hard'}>Hard</NavLink>
+        </MyButton>
       </CardActions>
     </Card>
   );
 }
 
+// TO DO : try the Expansion Panels to hide the level of difficluty and show them when clicking on the catégory cards
+// https://material-ui.com/demos/expansion-panels/
 export default CategoryCard;
